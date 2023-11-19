843d9434e6a7843d2aa275df9e87bc9a890208b9
NuzGooL <ardeniaanderland@gmail.com>
2023-11-19 14:50:33 +0300

    Initial commit

---
+++ src/main/java/JavaExam/Coursework2/ExaminatorApplication.java
@@ -0,0 +1,13 @@
+package JavaExam.Coursework2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaminatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JavaExam/Coursework2/ExaminatorApplicationTests.java
@@ -0,0 +1,13 @@
+package JavaExam.Coursework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
